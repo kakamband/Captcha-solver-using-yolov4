348450ff2f362365dfca3457d6a421afab92a7cb
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2020-11-12 11:38:09 +0530

    different apis for captcha to text and detections on image

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import os
 from yolov4_detection import detect
 import numpy as np
@@ -8,14 +8,13 @@ from img_treatments import treatment1, treatment4
 UPLOADED_IMGS_DIR = 'uploaded images/'
 UPLOADED_TREATED_DIR = 'uploaded treated/'
 YOLO_DATA_DIR = 'yolo-data'
-MIN_CONFIDENCE  = 0.5
+MIN_CONFIDENCE = 0.5
 NON_MAXIMA_THRESHOLD = 0.3
 
 
 app = Flask(__name__)
 
 
-@app.route("/CAPTCHA/yolo_detection", methods=["POST"])
 def predict():
     images = request.files.getlist("images")
 
@@ -32,14 +31,31 @@ def predict():
         cv2.imwrite(os.path.join(UPLOADED_TREATED_DIR, name), treated4)
 
     detections = {}
+    detected_imgs = []
     for name in img_names:
         treated_img_path = os.path.join(UPLOADED_TREATED_DIR, name)
-        result, _ = detect(treated_img_path, YOLO_DATA_DIR, MIN_CONFIDENCE, NON_MAXIMA_THRESHOLD)
+        result, detected_img = detect(treated_img_path, YOLO_DATA_DIR, MIN_CONFIDENCE, NON_MAXIMA_THRESHOLD)
         result = [value[0] for value in result]
         result = "".join(result)
         detections[name] = result
 
+        _, encoded_img = cv2.imencode('.png', detected_img)
+        detected_img = encoded_img.tostring()
+        detected_imgs.append(detected_img)
+
+    return detections, detected_imgs
+
+
+@app.route("/CAPTCHA/text", methods=["POST"])
+def captcha_to_text():
+    detections, _ = predict()
     return detections
+
+
+@app.route("/CAPTCHA/image", methods=["POST"])
+def captcha_img_detection():
+    _, detected_img = predict()
+    return Response(response=detected_img, mimetype='image/png')
 
 
 if __name__ == "__main__":
